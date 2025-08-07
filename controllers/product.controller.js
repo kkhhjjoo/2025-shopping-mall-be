8002302acbd6cb825ff60737d000078c01d4f89f
hyunjoo <khjoo1203@gmail.com>
2025-08-07 17:37:09 +0900

    상품 수정하기

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,7 @@
 const Product = require('../models/Product');
 
+const PAGE_SIZE = 1;
+
 const productController = {};
 
 productController.createProduct = async (req, res) => {
@@ -31,14 +33,42 @@ productController.createProduct = async (req, res) => {
   } catch (error) {
     res.status(400).json({ status: 'fail', error: error.message });
   }
-  
 };
 productController.getProducts = async (req, res) => {
-    try{
-      const products = await Product.find({});
-      res.status(200).json({status: 'success', data: products});
-    }catch(error){
-      res.status(400).json({ status: 'fail', error: error.message });
+  try {
+    const { page, name } = req.query;
+    const cond =
+      name && name.trim() !== ''
+        ? { name: { $regex: name, $options: 'i' } }
+        : {};
+    let query = Product.find(cond);
+    const response = {};
+    const pageNum = parseInt(page, 10);
+    if (pageNum && pageNum > 0) {
+      query.skip((pageNum - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      const totalItemNum = await Product.countDocuments(cond);
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
     }
+    const productList = await query.exec();
+    response.data = productList;
+    res.status(200).json({ status: 'success', ...response });
+  } catch (error) {
+    console.error(error); // 에러를 콘솔에 출력
+    res.status(400).json({ status: 'fail', error: error.message });
   }
+};
+productController.updateProduct = async (req,res) => {
+  try{
+    const productId = req.params.id;
+    const {sku, name, size, image, price, description, category, stock, status} = req.body;
+    const product = await Product.findByIdAndUpdate({_id:productId}, {sku, name, size, image, price, description, category, stock, status},
+      {new: true}
+    );
+    if(!product) throw new Error('아이템이 존재하지 않습니다');
+    res.status(200).json({status: 'success', data: product});
+  }catch(error) {
+    res.status(400).json({ status: 'fail', error: error.message });
+  }
+}
 module.exports = productController;
