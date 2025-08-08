df7e2317e7edb2992c27ef8f5b728cac9735e43b
hyunjoo <khjoo1203@gmail.com>
2025-08-08 16:47:54 +0900

    히로쿠 배포 수정

--- app.js
+++ app.js
@@ -7,15 +7,20 @@ const app = express();
 
 require('dotenv').config();
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); //req.body가 객체로 인식이 됩니다.
 
-app.use('/api',indexRouter);
+app.get('/', (req, res) => {
+  res.send('Welcome to the Shopping Mall API!');
+});
+
+app.use('/api', indexRouter);
 const mongoURI = process.env.LOCAL_DB_ADDRESS;
-mongoose.connect(mongoURI, {useNewUrlParser:true})//
-.then(()=>console.log('mongoose connected'))//
-.catch((error)=>console.log('DB connection fail', err));
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true }) //
+  .then(() => console.log('mongoose connected')) //
+  .catch((error) => console.log('DB connection fail', err));
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log('server on');
-}) 
+  console.log('server on');
+});
